a616fc87c25174736f9245a2e58c7b4c7bf069e6
SunnyJoe2000 <sunnyjoe2000@qq.com>
2017-11-02 21:18:02 +0800

    C language study

---
+++ The Simple Use Of C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  The Simple Use Of C
+//
+//  Created by mac on 17/11/2.
+//  Copyright © 2017年 SunnyJoe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
